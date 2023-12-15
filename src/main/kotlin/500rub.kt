989938a406039b79d5ae687639674d229991cb3b
PATYA <patysik@mail.ru>
2023-12-15 20:41:26 +0300

    из 2го урока  1. сделала код более компактным

--- src/main/kotlin/500rub.kt
+++ src/main/kotlin/500rub.kt
@@ -1,23 +1,28 @@
 import java.util.*
 fun main(){
 
-while (true) {
+while (true)
+{
     println("How much money do you have ?")
     val howMuchMoney = Scanner(System.`in`).nextInt()
+    val rich = howMuchMoney > 500
+    val notRich = howMuchMoney in 300..500
+    val medium = howMuchMoney in 100..299
+    val poor = howMuchMoney in 0..99
 
-    if (howMuchMoney > 500) {
-        println("You can buy a pizza !")
-    } else if (300 <= howMuchMoney && howMuchMoney <= 500) {
-        println("You can buy a pasta ! ")
-    } else if (100 <= howMuchMoney && howMuchMoney < 300) {
-        println("You can buy a hamburger !")
-    } else if (howMuchMoney < 100 && howMuchMoney > 0) {
-        println("You can buy pamen !")
-    } else {
-        println("Go to foodbank !")
+
+    if (rich)
+    { println("You can buy a pizza !")
+    } else if (notRich)
+    { println("You can buy a pasta ! ")
+    } else if (medium)
+    { println("You can buy a hamburger !")
+    } else if (poor)
+    { println("You can buy pamen !")
+    } else
+    { println("Go to foodbank !")
         break
     }
-
     println()
     println()
 }
