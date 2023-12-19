4334c471f931775eaa940153fb3c54549ec4ba46
Linka <Polina153@mail.ru>
2023-12-19 14:30:32 +0400

    FIXME added

--- src/main/kotlin/weather.kt
+++ src/main/kotlin/weather.kt
@@ -1,24 +1,23 @@
 import java.util.*
-fun main(){
+
+fun main() {
 
     println("What the time ?")
     val scannerTime = Scanner(System.`in`).nextInt()
     println("What the weather (good/bad) ?")
     val scannerWeather = Scanner(System.`in`).next().toLowerCase()
     val night = scannerTime in 21..24 || scannerTime in 1..6
-    val anyWeather = scannerWeather=="good" || scannerWeather=="bad"
+    val anyWeather = scannerWeather == "good" || scannerWeather == "bad"
     val day = scannerTime in 7..20
     val goodWeather = scannerWeather == "good"
 
-
-    if (night && anyWeather)
-    { println("Go to bed !")
-    }
-    else if ( day && goodWeather)
-    { println(" Go to walk !")
-    }
-    else
-    { println("To read a book !")
+//FIXME убрать лишнюю переменную
+    if (night && anyWeather) {
+        println("Go to bed !")
+    } else if (day && goodWeather) {
+        println(" Go to walk !")
+    } else {
+        println("To read a book !")
     }
 }
 
