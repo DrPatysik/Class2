e2458148f130ed6551e60f216103ab06c43e9f86
PATYA <patysik@mail.ru>
2023-12-22 02:20:29 +0300

    добавила do-while

--- src/main/kotlin/scanner.seconds.kt
+++ src/main/kotlin/scanner.seconds.kt
@@ -1,18 +1,31 @@
 import java.util.*
 
-fun main() {
-        println("Сколько секунд ?")
-        val scanner = Scanner(System.`in` ).nextInt()
-        val oneDayInSeconds = 24 * 60 * 60
-        val days = scanner / oneDayInSeconds
-        val oneHourInSeconds = 60 * 60
-        val hours = ( scanner % oneDayInSeconds ) / oneHourInSeconds
-        val minutes = (( scanner % oneDayInSeconds ) % oneHourInSeconds ) / 60
-        val leftSeconds = scanner - days * oneDayInSeconds - hours * oneHourInSeconds - minutes * 60
-//    вывод в консоль
-        println("Общее количество дней: $days !")
-        println("Общее количество часов: $hours !")
-        println("Общее количество минут: $minutes !")
-        println("Оставшееся количество секунд: $leftSeconds !")
+fun main()
+{
+        do
+        {
+                println("Сколько секунд ?")
+                val howMuchSeconds = Scanner(System.`in`).nextLong()
+                val oneDayInSeconds = 24 * 60 * 60
+                val days = howMuchSeconds / oneDayInSeconds
+                val oneHourInSeconds = 60 * 60
+                val hours = (howMuchSeconds % oneDayInSeconds) / oneHourInSeconds
+                val minutes = ((howMuchSeconds % oneDayInSeconds) % oneHourInSeconds) / 60
+                val leftSeconds = howMuchSeconds - days * oneDayInSeconds - hours * oneHourInSeconds - minutes * 60
+
+                println("Общее количество дней: $days !")
+                println("Общее количество часов: $hours !")
+                println("Общее количество минут: $minutes !")
+                println("Оставшееся количество секунд: $leftSeconds !")
+                println()
+                println(" Хотите продолжить?(да/нет)")
+                val agree = Scanner(System.`in`).next().toLowerCase() == "да"
+                println()
+
+        }
+        while ( agree )
+        println()
+        println(" До свидания!")
+
 }
 
