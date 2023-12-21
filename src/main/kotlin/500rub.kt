fec2c61cf95ad5fa1583fcf06dbd5f8624af83a9
PATYA <patysik@mail.ru>
2023-12-21 23:23:06 +0300

    добавила do-while

--- src/main/kotlin/500rub.kt
+++ src/main/kotlin/500rub.kt
@@ -1,34 +1,35 @@
 import java.util.*
-fun main(){
-
-while (true)
+fun main()
 {
-    println("How much money do you have ?")
-    val howMuchMoney = Scanner(System.`in`).nextInt()
-    val ifRich = howMuchMoney > 500
-    val ifNotRich = howMuchMoney in 300..500
-    val ifMedium = howMuchMoney in 100..299
-    val ifPoor = howMuchMoney in 0..99
+    do {
+        println("How much money do you have ?")
+        val howMuchMoney = Scanner(System.`in`).nextInt()
+        val ifRich = howMuchMoney in 500..1000_000
+        val ifNotRich = howMuchMoney in 300..500
+        val ifMedium = howMuchMoney in 100..299
+        val ifPoor = howMuchMoney in 1..99
 
+        if (ifRich)
+        { println("You can buy a pizza !")
+        } else if (ifNotRich)
+        { println("You can buy a pasta ! ")
+        } else if (ifMedium)
+        { println("You can buy a hamburger !")
+        } else if (ifPoor)
+        { println("You can buy pamen !")
+        } else
+        { println( "Sorry,dude,this is not your day!")
+        }
+        println()
 
-    if (ifRich)
-    { println("You can buy a pizza !")
-    } else if (ifNotRich)
-    { println("You can buy a pasta ! ")
-    } else if (ifMedium)
-    { println("You can buy a hamburger !")
-    } else if (ifPoor)
-    { println("You can buy pamen !")
-    } else
-    { println("Go to foodbank !")
-        break
+        println("Do you want to continue?(yes/no)")
+        val ifContinue = Scanner(System.`in`).next().toLowerCase()
+        val positive = ifContinue == "yes"
+        println()
+
     }
-    println()
-    println()
+    while( howMuchMoney in 1 .. 1000_000 && positive
+           )
+    println("See you later!")
 }
-println("See you later!")
-}
-
-
-
 
