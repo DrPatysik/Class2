c8939e291736c50900067cab41b0ee54a1d90156
PATYA <patysik@mail.ru>
2023-12-21 15:08:35 +0300

    убрала лишнюю переменную

--- src/main/kotlin/weather.kt
+++ src/main/kotlin/weather.kt
@@ -6,15 +6,14 @@ fun main() {
     val scannerTime = Scanner(System.`in`).nextInt()
     println("What the weather (good/bad) ?")
     val scannerWeather = Scanner(System.`in`).next().toLowerCase()
-    val night = scannerTime in 21..24 || scannerTime in 1..6
-    val anyWeather = scannerWeather == "good" || scannerWeather == "bad"
-    val day = scannerTime in 7..20
-    val goodWeather = scannerWeather == "good"
+    val ifNight = scannerTime in 21..24 || scannerTime in 1..6
+    val ifDay = scannerTime in 7..20
+    val ifGoodWeather = scannerWeather == "good"
 
 //FIXME убрать лишнюю переменную
-    if (night && anyWeather) {
+    if (ifNight ) {
         println("Go to bed !")
-    } else if (day && goodWeather) {
+    } else if (ifDay && ifGoodWeather) {
         println(" Go to walk !")
     } else {
         println("To read a book !")
