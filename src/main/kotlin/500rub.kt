ee4459f6f2d965b69d861c21769754e9e4def3c9
PATYA <patysik@mail.ru>
2024-01-06 15:26:30 +0300

    добавлены функции и when

--- src/main/kotlin/500rub.kt
+++ src/main/kotlin/500rub.kt
@@ -2,31 +2,30 @@ import java.util.*
 fun main()
 {
     do {
-        println("How much money do you have ?")
-        val howMuchMoney = Scanner(System.`in`).nextInt()
-        val ifRich = howMuchMoney in 500..1000_000
-        val ifNotRich = howMuchMoney in 300..500
-        val ifMedium = howMuchMoney in 100..299
-        val ifPoor = howMuchMoney in 1..99
-
-        if (ifRich)
-            { println("You can buy a pizza !") }
-        else if (ifNotRich)
-             { println("You can buy a pasta ! ") }
-        else if (ifMedium)
-            { println("You can buy a hamburger !") }
-        else if (ifPoor)
-            { println("You can buy ramen !") }
-        else
-            { println( "Sorry,dude,this is not your day!") }
-        println()
+        val quantoMoney = getMoney()
+        chooseFood(quantoMoney)
 
         println("Do you want to continue?(yes/no)")
         val wantContinue = Scanner(System.`in`).next().toLowerCase()
         println()
-
     }
-    while( howMuchMoney in 1 .. 1000_000 && wantContinue =="yes")
-        println("See you later!")
+    while( quantoMoney in 1 .. 1000_000 && wantContinue =="yes")
+        println("See you later!Bye-bye!")
 }
 
+
+fun getMoney():Int
+{  println("How much money do you have ?")
+    val howMuchMoney = Scanner(System.`in`).nextInt()
+    return howMuchMoney
+}
+
+fun chooseFood(money:Int)
+{   when(money)
+    { in 1..99 -> println("You can buy ramen !")
+    in 100..299 -> println("You can buy a hamburger !")
+    in 300..500 -> println("You can buy a pasta ! ")
+    in 500..1_000_000 -> println("You can buy a pizza !")
+    else -> println( "Sorry,dude,this is not your day!")
+    }
+}
